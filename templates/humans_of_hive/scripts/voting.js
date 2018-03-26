b5116bc370b0ce6d1e9f88e1babcfc716dce1cc1
Allan Petrie <2254840P@student.gla.ac.uk>
2018-03-26 13:14:10 +0100

    fixed untracked files

---
+++ templates/humans_of_hive/scripts/voting.js
@@ -0,0 +1,10 @@
+function upvote(){
+  window.alrert("upvote")
+}
+
+function downvote(){
+  window.alert("downvote")
+}
+
+document.getElementById("Upvote").onclick = upvote;
+document.getElementById("Downvote").onclick = downvote;
